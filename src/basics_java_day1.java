e5a16526ac0dcd641f30d80c66174a33a4bea725
Soham Sarkar <srutidhara153@gmail.com>
2025-06-10 10:12:54 +0530

    1st-Day-Basics

--- src/basics_java_day1.java
+++ src/basics_java_day1.java
@@ -1,4 +1,15 @@
+/*
+Input a year and find whether it is a leap year or not.
+Take two numbers and print the sum of both.
+Take a number as input and print the multiplication table for it.
+Take 2 numbers as inputs and find their HCF and LCM.
+Keep taking numbers as inputs till the user enters ‘x’, after that print sum of all.
+ */
+
 public class basics_java_day1 {
+    public static void main(String[] args) {
+
+    }
 
 
 }
